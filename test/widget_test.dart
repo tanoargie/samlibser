59dd2e44966a0b741df0401d429ea80c5d3db705
Franco Serio <francoagustinserio@gmail.com>
2023-09-18 20:26:47 -0300

    test compiling at least

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:samlibser/main.dart';
+import 'package:samlibser/app/view/app.dart';
+import 'package:authentication_repository/authentication_repository.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final authenticationRepository = AuthenticationRepository();
+    await tester.pumpWidget(App(
+      authenticationRepository: authenticationRepository,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
