107350ad3412240473523d11466ca788b26f32bc
Franco Serio <francoagustinserio@gmail.com>
2023-09-27 16:51:04 -0300

    now login page appears in web too

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:samlibser/app/view/app.dart';
 import 'package:samlibser/app/bloc/app_bloc.dart';
+import 'package:samlibser/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const AppBlocObserver();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final authenticationRepository = AuthenticationRepository();
   await authenticationRepository.user.first;
