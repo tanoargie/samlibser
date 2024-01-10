ecb958b4f249c365574d0a2ad96ac923eecb4e4d
Franco Serio <franco@samser.co>
2024-01-10 20:46:47 -0300

    feat: Add url_launcher for terms

--- lib/login/view/login.dart
+++ lib/login/view/login.dart
@@ -1,7 +1,9 @@
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:samlibser/login/view/login_form.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:samlibser/login/cubit/login_cubit.dart';
 
 class LoginPage extends StatelessWidget {
@@ -17,12 +19,37 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: BlocProvider(
-          create: (_) => LoginCubit(context.read<AuthenticationRepository>()),
-          child: const LoginForm(),
-        ),
+      body: Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(16),
+              child: BlocProvider(
+                create: (_) =>
+                    LoginCubit(context.read<AuthenticationRepository>()),
+                child: const LoginForm(),
+              )),
+          Center(
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  const TextSpan(
+                    text: 'When logged/signed up you accept our ',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  TextSpan(
+                    text: 'terms',
+                    style: const TextStyle(color: Colors.blue),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        launchUrl(Uri.parse(
+                            'https://samlibser.samser.co/terms/terms.pdf'));
+                      },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
