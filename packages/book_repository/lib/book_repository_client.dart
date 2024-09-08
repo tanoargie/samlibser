3b3fc0c28f04f7947d15a5e8c8a8b489930a2503
Franco Serio <franco@samser.co>
2024-09-08 19:18:39 +0200

    fix: fucking works finally

--- packages/book_repository/lib/book_repository_client.dart
+++ packages/book_repository/lib/book_repository_client.dart
@@ -1,9 +1,17 @@
+import 'dart:async';
 import 'package:epubx/epubx.dart';
 import 'package:googleapis/drive/v3.dart' as drive;
 
 class BookRepositoryClient {
   static Future<EpubBook> getEpubFile(drive.Media mediaFile) async {
-    final data = await mediaFile.stream.first;
-    return EpubReader.readBook(data);
+    List<int> datastore = [];
+    final Completer<EpubBook> completer = new Completer<EpubBook>();
+    await mediaFile.stream.listen((data) {
+      datastore.insertAll(datastore.length, data);
+    }, onDone: () async {
+      final book = await EpubReader.readBook(datastore);
+      completer.complete(book);
+    });
+    return completer.future;
   }
 }
